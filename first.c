a8d74a9b8284aa1ae90780e902a58e136501a028
Admin <rakeshchowdary1045@gmail.com>
2021-03-18 12:09:42 +0530

    4 files are added in first commit

---
+++ first.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//#include<conio.h>
+void main ()
+{
+	int a,b,c;
+	printf("Enter the two numbers a and b : ");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("The sum of the two numbers is : %d ",c);
+}
